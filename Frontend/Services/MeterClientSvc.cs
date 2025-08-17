2f735f1694503ffd6281f8c00a091979f430427a
Will <williamjweston@outlook.com>
2025-08-17 15:59:17 +0100

    Initail Commit

---
+++ Frontend/Services/MeterClientSvc.cs
@@ -0,0 +1,17 @@
+﻿namespace Frontend.Services
+{
+    public class MeterClientSvc
+    {
+        public readonly HttpClient _httpClient;
+
+        public MeterClientSvc(HttpClient httpClient) 
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task UploadMeterReadings()
+        {
+            var result = _httpClient.P
+        }
+    }
+}
